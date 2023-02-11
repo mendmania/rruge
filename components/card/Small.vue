fbb256d0bcdf374f761deea93099ae5caa8cc175
mendmania <mendim@gjirafa.com>
2023-02-11 17:31:57 +0100

    [added] conection with describedlife api

--- components/card/Small.vue
+++ components/card/Small.vue
@@ -1,11 +1,24 @@
 <script setup>
+import { toRefs } from 'vue';
 console.log("article-sm");
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true
+  }
+})
+
 </script>
 
 <template>
   <article class="m-card m-card-sm">
-    <nuxt-link :to="'/article-url'">
-      Media measurement uncertainty—tracking TV, social and digital
+    <nuxt-link :to="slug">
+      {{title}}
     </nuxt-link>
 
     <time class="text-xsm" datetime="2017-10-31T11:21:00+02:00">Tuesday, 31 October 2017</time>
--- pages/[url].vue
+++ pages/[url].vue
@@ -1,14 +1,55 @@
 <script setup>
+const { data: singlePost, error } = await useAsyncData("cafes", async () => {
+  const route = useRoute();
+  console.log("cucamalevev", route.params.url);
+  console.log("cucamalevev", route.fullPath);
+  const slug = route.params.url;
+
+  const [post] = await $fetch(
+    `/posts?slug=${slug}`,
+    {
+      method: "GET",
+      baseURL: "https://describedlife.com/wp-json/wp/v2",
+    }
+  );
+
+  const thumbnail = await $fetch(
+    `${post["_links"]["wp:featuredmedia"][0].href}`,
+    {
+      method: "GET",
+    }
+  );
+
+  return { post, thumbnail };
+});
+
+// const { data: thumbnail } = await useAsyncData("cafes", () => {
+
+//   const slug = route.params.url
+
+//   return $fetch(`${singlePost[0]['_links']['wp:featuredmedia'][0].href}`, {
+//     method: "GET",
+//   });
+// });
 </script>
 
 <template>
   <div class="m-page m-page-article">
-
     <SectionLarge>
-        <ArticleMain />
+
+      <!-- <pre>
+      {{ singlePost.thumbnail.media_details.sizes.thumbnail.source_url }}
+    </pre> -->
+
+      <ArticleMain
+        :title="singlePost.post.title.rendered"
+        :content="singlePost.post.content.rendered"
+        :date="singlePost.post.date"
+        :thumbnail="singlePost.thumbnail.media_details.sizes.thumbnail.source_url"
+      />
     </SectionLarge>
     <SectionSmall>
-        <CardMedium />
+      <CardMedium />
     </SectionSmall>
   </div>
 </template>
